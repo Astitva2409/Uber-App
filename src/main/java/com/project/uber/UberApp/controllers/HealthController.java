47146661747a786a7b0712cb4a49568936269c0d
Astitva Singh <singhastitva99@gmail.com>
2026-03-02 13:09:29 +0530

    updated the health controller

--- src/main/java/com/project/uber/UberApp/controllers/HealthController.java
+++ src/main/java/com/project/uber/UberApp/controllers/HealthController.java
@@ -1,5 +1,6 @@
 package com.project.uber.UberApp.controllers;
 
+import com.project.uber.UberApp.advices.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/")
-    public ResponseEntity<String> getHealth() {
-        return ResponseEntity.ok("Ok");
+    public ResponseEntity<ApiResponse<String>> getHealth() {
+        return ResponseEntity.ok(new ApiResponse<>("Ok"));
     }
 }
